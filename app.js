4043de3251102e411fd46e81b7b08b142ed70be5
FranciscoNevesT <FranciscoNevesT@outlook.com>
2022-04-29 19:51:07 -0300

    Formulario quase finalizado

--- app.js
+++ app.js
@@ -1,34 +1,26 @@
 //Configurações do servidor
-
+const fs = require("fs");
 
 const express = require('express');
 const app = express();
-const path = require('path');
-const controleBanco = require("./public/js/controleBanco");
-
-
-//Configurações do banco de dados
-const sequelize = require(`${__dirname}/models/models`)
-
 
 
 app.use(express.static('.'))
 
+var formularioRouter = require("./routes/formulario");
+//Configurações do banco de dados
 
 //Rota "catchall" de teste 
-app.get('/formulario', (req, res) => {
-    res.sendFile(path.resolve(__dirname,'./views/formulario.html'));
-});
+
+app.use("/formulario",formularioRouter);
 
 
 //Rota "catchall" de teste 
 app.get('/', (req, res) => {
+
     res.send(`<h1>Dummy Page</h1>
             <a href="/formulario">Pagina de formulario</a>`);
 });
-
-
-controleBanco.getCitacao(sequelize,7);
 
 //Inicializando servidor
 app.listen(3000);
