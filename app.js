a76eb55a5d983106bbc2462e88d8a53f2b7c4456
henriquemoc <henriquemoc2@gmail.com>
2022-05-02 01:44:35 -0300

    Mudando app para colocar rota de busca

--- app.js
+++ app.js
@@ -4,17 +4,21 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 
-
-app.use(express.static('.'))
+app.use(express.static('.'));
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 // Rotas
 var indiceRouter = require("./routes/indice");
 var getArtigosRouter = require("./routes/getArtigos");
 var formularioRouter = require("./routes/formulario");
+var searchRouter = require("./routes/busca");
 
 app.use("/formulario",formularioRouter);
 app.use("/", indiceRouter);
 app.use("/getarticles", getArtigosRouter);
+app.use("/buscar", searchRouter);
 
 
 //Rota "catchall" de teste 
