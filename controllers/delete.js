fe25e20327dc7382ea409e752f6528fa0f3e7054
Thiago Henrique <thiagosantosgg0@gmail.com>
2022-05-20 18:49:34 -0300

    Removendo Artigos

--- controllers/delete.js
+++ controllers/delete.js
@@ -1,10 +1,24 @@
+const path = require('path');
+const Sequelize = require('sequelize');
+const backdirname = path.dirname(__dirname);
+const sequelize = require(`${backdirname}/models/models`);
 
 
 
 
 exports.postDeleteArticle = ((req, res, err) => {
     console.log("Trigger de remoção realizado.\n");
-    console.log("Lógica de remoção: TBD");
-    // const { articleId } = req.body;
-    console.log(req.body);
+    console.log("Lógica de remoção: TBD\n");
+    const { articleId } = req.body;
+    console.log("ArticleID: " + articleId);
+
+    sequelize.models.Artigo.findByPk(articleId)
+    .then((articleToBeDeleted) =>{
+        articleToBeDeleted.destroy();
+        console.log(articleToBeDeleted);
+    })
+    .then((result) => {
+        console.log("Artigo deletado com sucesso!");
+        res.redirect('/');
+    });
 });
--- routes/deleteArticle.js
+++ routes/deleteArticle.js
@@ -1,13 +1,6 @@
-const path = require('path');
 const express = require('express');
-const Sequelize = require('sequelize');
-
-const backdirname = path.dirname(__dirname);
 var router = express.Router();
-const deleteController = require('../controllers/delete');
-
-const sequelize = require(`${backdirname}/models/models`);
-
+const deleteController = require('../controllers/delete.js');
 
 router.post('/deletearticle', deleteController.postDeleteArticle);
 module.exports = router;
