94c458c7567b94e4393c28d9a3a5d5f0876bffcf
henriquemoc <henriquemoc2@gmail.com>
2022-05-20 20:07:22 -0300

    Consertando busca

--- routes/busca.js
+++ routes/busca.js
@@ -7,15 +7,20 @@ var searchRouter = express.Router();
 
 const sequelize = require(`${backdirname}/models/models`);
 
-searchRouter.get('/search', async function(req, res, next) {
+searchRouter.get('/search', async function(req, res, next){
     let { term } = req.query;
     term = term.toLowerCase();
-
-    var query = "Select Artigos.ID, Artigos.NOME as TITULO, Autors.NOME, Artigos.DATA_PUBLI, Artigos.DOWNLOADS From Artigos inner join Autors on Artigos.Autor_ID = Autors.ID;"
-    const [arts, metadata] = await sequelize.query(query);
+    search_term = '%'+term+'%';
+    console.log(search_term);
+    //var query = "Select Artigos.ID, Artigos.NOME as TITULO, Autors.NOME, Artigos.DATA_PUBLI, Artigos.DOWNLOADS From Artigos inner join Autors on Artigos.Autor_ID = Autors.ID WHERE TITULO LIKE :search_name;"
+    const [arts, metadata] = await sequelize.query(
+      "Select Artigos.ID, Artigos.NOME as TITULO, Autors.NOME, Artigos.DATA_PUBLI, Artigos.DOWNLOADS From Artigos inner join Autors on Artigos.Autor_ID = Autors.ID WHERE TITULO LIKE :search_name",
+      {
+        replacements: { search_name: search_term }
+      }
+    );
     console.log(arts[0]);
   	res.render(path.resolve(backdirname,'./views/resultadoBusca.html'), {arts});
-
   });
 
 module.exports = searchRouter;
