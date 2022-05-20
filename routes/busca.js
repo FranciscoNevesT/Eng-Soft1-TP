aa33c0a6d7efc10147aeda37456171253a3265e4
henriquemoc <henriquemoc2@gmail.com>
2022-05-20 19:29:03 -0300

    Mudando método de busca

--- routes/busca.js
+++ routes/busca.js
@@ -7,14 +7,15 @@ var searchRouter = express.Router();
 
 const sequelize = require(`${backdirname}/models/models`);
 
-searchRouter.get('/search', (req, res) => {
+searchRouter.get('/search', async function(req, res, next) {
     let { term } = req.query;
     term = term.toLowerCase();
 
-    artigos = sequelize.models.Artigo;
+    var query = "Select Artigos.ID, Artigos.NOME as TITULO, Autors.NOME, Artigos.DATA_PUBLI, Artigos.DOWNLOADS From Artigos inner join Autors on Artigos.Autor_ID = Autors.ID;"
+    const [arts, metadata] = await sequelize.query(query);
+    console.log(arts[0]);
+  	res.render(path.resolve(backdirname,'./views/resultadoBusca.html'), {arts});
 
-    artigos.findAll({ where: { NOME: { [Sequelize.Op.like]: '%' + term + '%' } } })
-      .then(arts => res.render(path.resolve(backdirname,'./views/resultadoBusca.html'), {arts}));
   });
 
 module.exports = searchRouter;
