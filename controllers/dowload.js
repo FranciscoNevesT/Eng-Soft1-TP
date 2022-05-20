5088fd6f21fac1e4ca6271a91f831c73951fadda
FranciscoNevesT <FranciscoNevesT@outlook.com>
2022-05-20 16:21:25 -0300

    Downloads incrementado

--- controllers/dowload.js
+++ controllers/dowload.js
@@ -1,18 +1,28 @@
 const fs = require('fs')
 
+const sequelize = require(`../models/models`);
+
 exports.postDeleteArticle = ((req, res, err) => {
     console.log("Trigger de dowload realizado.\n");
     // const { articleId } = req.body;
     console.log(req.body);
 
+    artigos = sequelize.models.Artigo;
+    console.log(artigos)
+
     path = "artigos/"+req.body.articleId+".pdf";
 
     try {
         if (fs.existsSync(path)) {
-            res.download(path);
+            artigos.increment("DOWNLOADS", {by: 1,
+              where: {ID : req.body.articleId}
+            })
+            res.download(path)
         }
         else{
-            res.redirect("/");
+          
+         res.redirect("/");
+          
         }
       } catch(err) {
         console.error(err)
