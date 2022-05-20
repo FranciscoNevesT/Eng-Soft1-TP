8e8cf3b0a6322299642865888bc80cdad1601259
hfscheid <hfscheid@gmail.com>
2022-05-20 16:39:10 -0300

    corrigida exibição de artigos no indice

--- routes/indice.js
+++ routes/indice.js
@@ -7,10 +7,17 @@ const backdirname = path.dirname(__dirname);
 const indiceRouter = express.Router();
 const sequelize = require(`${backdirname}/models/models`);
 
-indiceRouter.get("", function(req, res, next) {
+//indiceRouter.get("", function(req, res, next) {
+indiceRouter.get("", async function(req, res, next) {
     artigos = sequelize.models.Artigo;
-    artigos.findAll().then(
-	arts=> res.render(path.resolve(backdirname,'./views/indice.html'), {arts}));
+    autores = sequelize.models.Autor;
+
+    var query = "Select Artigos.ID, Artigos.NOME as TITULO, Autors.NOME, Artigos.DATA_PUBLI, Artigos.DOWNLOADS From Artigos inner join Autors on Artigos.Autor_ID = Autors.ID;"
+    const [arts, metadata] = await sequelize.query(query);
+    console.log(arts[0]);
+  	res.render(path.resolve(backdirname,'./views/indice.html'), {arts});
+//    artigos.findAll().then(
+//	arts=> res.render(path.resolve(backdirname,'./views/indice.html'), {arts}));
 });
 
 module.exports = indiceRouter;
