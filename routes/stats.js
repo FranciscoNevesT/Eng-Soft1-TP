d65c6b2c65f3d8e598cca16fad5b96d420cbf439
henriquemoc <henriquemoc2@gmail.com>
2022-05-22 18:57:55 -0300

    Adicionando estatística Download por data

--- routes/stats.js
+++ routes/stats.js
@@ -11,7 +11,21 @@ const sequelize = require(`${backdirname}/models/models`);
 statsRouter.get("/stats/:id", async function(req, res, next) {
     artigos = sequelize.models.Artigo;
     autores = sequelize.models.Autor;
-    art = await artigos.findAll({ where: { ID: id } });
+    art = await artigos.findAll({ where: { ID: req.params.id } });
+    console.log(art[0].ID)
+    const [data, metadatatime] = await sequelize.query(
+        "Select Downloads.DATA From Downloads WHERE Downloads.Artigo_ID LIKE :art_id",
+        {
+          replacements: { art_id: art[0].ID }
+        }
+    );
+    var downloadPerMonth = [0,0,0,0,0,0,0,0,0,0,0,0];
+    for (var i = 0; i < data.length; i++) {
+        let monthMinusOne = parseInt(data[i].DATA.split("-")[1]) - 1;
+        downloadPerMonth[monthMinusOne] = downloadPerMonth[monthMinusOne] + 1;
+        // more statements
+    }
+    console.log(downloadPerMonth);
     var query = "Select Artigos.ID, Artigos.NOME as TITULO, Autors.NOME, Artigos.DATA_PUBLI, Artigos.DOWNLOADS From Artigos inner join Autors on Artigos.Autor_ID = Autors.ID;"
     const [result, metadata] = await sequelize.query(query);
 //    stats = {
@@ -23,7 +37,7 @@ statsRouter.get("/stats/:id", async function(req, res, next) {
 //    };
 
 
-  	res.render(path.resolve(backdirname,'./views/indice.html'), {stats});
+  	res.render(path.resolve(backdirname,'./views/stats.html'), {downloadPerMonth:downloadPerMonth});
 });
 
 module.exports = statsRouter;
